1f8e0caab7c2a5ef9ca8cb1416341eed92986154
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2013-06-05 16:24:48 +0200

    Fixed navbar

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -1,2 +1,34 @@
 //= require jquery
 // =require modernizr
+
+// Fixed navbar onScroll action
+$(window).bind("load", function() {
+
+  var nav_position = $("#scroll_menu").offset().top;
+
+  var sticky_navbar = function(){
+    var position = $(document).scrollTop();
+
+    if (nav_position < position) {
+      $("#scroll_menu").addClass("navfixed");
+    } else {
+      $("#scroll_menu").removeClass("navfixed");
+    }
+  };
+
+  sticky_navbar();
+
+  $(window).scroll(function() {
+    sticky_navbar();
+  });
+
+  // smooth scroll to anchor
+  $('.legend > a').on('click', function(){
+    var height_navbar = "100px";
+
+    $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
+});
