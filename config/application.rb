e9079cec838cb2826465c5b76a784bbd6a7091a2
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2014-12-15 13:19:52 +0100

    Reset email to metroouchy

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@
 #
 # COMMAND
 #
-COMMAND_EMAIL_TO_OUCHY = 'ludovic.turmel@liquid-concept.ch'
+COMMAND_EMAIL_TO_OUCHY = 'metro-ouchy@ovan.ch'
 COMMAND_EMAIL_TO_FLON = 'metro-flon@ovan.ch'
 COMMAND_SUBJECT  = 'Contact depuis le site MertoOuchy / MetroFlon'
 
